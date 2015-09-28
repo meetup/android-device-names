1b1ee90f70a65b486b167b7d122061c668ccea79
mike castleman <mlc@meetup.com>
2015-09-28 12:00:39 -0400

    comment

--- fix_spacing.rb
+++ fix_spacing.rb
@@ -1,5 +1,8 @@
 #!/usr/bin/env ruby
 
+# install sponge from https://joeyh.name/code/moreutils/ and then run like
+# ruby fix_spacing.rb < android_model.properties | sponge android_model.properties
+
 $stdin.each_line do |l|
   raw, human = l.split('=', 2)
   raw += ' ' * (20 - raw.length) if raw.length < 20
